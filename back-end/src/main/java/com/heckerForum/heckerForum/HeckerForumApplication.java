0531ec697a35951530453c12e3186f9f2b41dd12
yiuheijeffre <yiuheijeffre@student.unimelb.edu.au>
2023-11-22 06:00:27 +1100

    Removed config logging
    Removed config logging

--- back-end/src/main/java/com/heckerForum/heckerForum/HeckerForumApplication.java
+++ back-end/src/main/java/com/heckerForum/heckerForum/HeckerForumApplication.java
@@ -12,7 +12,6 @@ public class HeckerForumApplication {
   public static void main(String[] args) {
     log.debug("Starting main");
     SpringApplication springApplications = new SpringApplication(HeckerForumApplication.class);
-    springApplications.addListeners(new PropertiesLogger());
     springApplications.run(args);
   }
 
