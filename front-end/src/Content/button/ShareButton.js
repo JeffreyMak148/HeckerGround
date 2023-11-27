b5a3b0043d2f063a2c5212322facfe0ea38c455e
yiuheijeffre <yiuheijeffre@student.unimelb.edu.au>
2023-11-27 20:51:35 +1100

    Add fade effect to share popover
    Add fade effect to share popover

--- front-end/src/Content/button/ShareButton.js
+++ front-end/src/Content/button/ShareButton.js
@@ -1,55 +1,51 @@
 import React, { useState } from 'react';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { BiSolidCopy } from 'react-icons/bi';
 import { BsShareFill } from 'react-icons/bs';
-import { ArrowContainer, Popover } from 'react-tiny-popover';
 import "../Content.css";
 
 export const ShareButton = ({postId, commentNum, title, commentModal}) => {
-    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
     const [value, setValue] = useState("");
 
     const setShareValue = () => {
         const content = `${!!title ? `【${title}】` : ""}\n`;
         const link = `${!!commentNum ? `https://heckerground.com/posts/${postId}?comment=${commentNum}` : `https://heckerground.com/posts/${postId}?page=1`}`;
         setValue(content+link);
-    }
+    };
+
+    const shareContent = (
+        <Popover id="share-popover" className="share-popover">
+            <Popover.Body className="share-popover-body">
+                <textarea className="share-text-area" readOnly value={value} />
+                <CopyToClipboard text={value}>
+                    <div className="share-copy-div">
+                        <BiSolidCopy size="1.5em" />
+                        Copy
+                    </div>
+                </CopyToClipboard>
+            </Popover.Body>
+        </Popover>
+    );
 
     return (
             !!postId ? 
-                <Popover
-                    containerClassName="share-popover"
-                    containerStyle={commentModal ? {zIndex: "1060"} : {zIndex: "6"}}
-                    padding={10}
-                    isOpen={isPopoverOpen}
-                    reposition={false}
-                    positions={['bottom', 'top', 'left']}
-                    onClickOutside={() => setIsPopoverOpen(false)}
-                    content={({ position, childRect, popoverRect }) => (
-                        <ArrowContainer
-                            position={position}
-                            childRect={childRect}
-                            popoverRect={popoverRect}
-                            arrowSize={10}
-                            arrowColor={'#43434f'}
-                            className="popover-arrow-container"
-                            arrowClassName="popover-arrow">
-                            <textarea className="share-text-area" readOnly value={value} />
-                            <CopyToClipboard text={value}>
-                                <div className="share-copy-div">
-                                    <BiSolidCopy size="1.5em" />
-                                    Copy
-                                </div>
-                            </CopyToClipboard>
-                        </ArrowContainer>
-                    )}>
-                    {
-                        !!commentNum ?
-                            <button className="content-share-button" onClick={() => {setIsPopoverOpen(!isPopoverOpen); setShareValue()}}><BsShareFill /></button>
-                        :
-                            <button className="share-button" onClick={() => {setIsPopoverOpen(!isPopoverOpen); setShareValue()}}><BsShareFill /></button>
-                    }
-                </Popover>
-            : <></>
+                    <OverlayTrigger
+                        trigger="click"
+                        rootClose
+                        placement="bottom"
+                        overlay={shareContent}
+                    >
+                        <span>
+                            {
+                                !!commentNum ?
+                                    <button className="content-share-button" onClick={setShareValue}><BsShareFill /></button>
+                                :
+                                    <button className="share-button" onClick={setShareValue}><BsShareFill /></button>
+                            }
+                        </span>
+                    </OverlayTrigger>
+                :
+                    <></>
     );
 };
