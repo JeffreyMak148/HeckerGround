e0c7cb1cad0029170162ba76a48a2edf4d260b09
JeffreyMak148 <107347234+JeffreyMak148@users.noreply.github.com>
2025-11-11 02:56:22 +1100

    Change AWS settings

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sudo scp application-prod.properties HeckerGround/back-end/src/main/resources/
 
 cd /home/ec2-user/HeckerGround
 
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 009160074177.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 470684059444.dkr.ecr.ap-southeast-2.amazonaws.com
 
 docker-compose -f docker-compose-ec2.yml down
 docker-compose -f docker-compose-ec2.yml up --pull always --build --force-recreate -d
