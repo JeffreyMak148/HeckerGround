31294c571b76743caaaf0451b3aebffc833dd600
JeffreyMak148 <107347234+JeffreyMak148@users.noreply.github.com>
2025-11-12 23:07:13 +1100

    Update CodeDeploy docker compose command

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ cd /home/ec2-user/HeckerGround
 
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 470684059444.dkr.ecr.ap-southeast-2.amazonaws.com
 
-docker-compose -f docker-compose-ec2.yml down
-docker-compose -f docker-compose-ec2.yml up --pull always --build --force-recreate -d
+docker compose -f docker-compose-ec2.yml down
+docker compose -f docker-compose-ec2.yml up --pull always --build --force-recreate -d
